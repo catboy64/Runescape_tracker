42da8a5e36a42a29a4ea29c9a555fc794e607baa
catboy64 <153577622+catboy64@users.noreply.github.com>
2023-12-12 23:08:28 +0000

    Update main.py
    oui oui

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import profile
+
+# Juste pour tester si le code marche, je vais ajouter un GUI dans le futur
+
+print("Skill #1: ",profile.s1.name)
+print("Level: ",profile.s1.lvl)
+print("XP: ",profile.s1.xp)
+print("Rank: ",profile.s1.rank)
